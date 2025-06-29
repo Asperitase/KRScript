5b6e3186c6620f9030386ed7e188a7e6711ec4a7
Asperatise <116976373+Asperitase@users.noreply.github.com>
2025-06-29 15:33:47 +0200

    test

--- modules/RobloxAPI.lua
+++ modules/RobloxAPI.lua
@@ -5,15 +5,13 @@ RobloxAPI.__index = RobloxAPI
 function RobloxAPI.new()
     local self = setmetatable({}, RobloxAPI)
     
-    -- Инициализация сервисов
     self.players = game:GetService("Players")
     self.replicated_storage = game:GetService("ReplicatedStorage")
     self.workspace = game:GetService("Workspace")
     
-    -- Инициализация переменных
     self.local_player = self.players.LocalPlayer
-    self.plots = self.workspace:WaitForChild("Plots"):WaitForChild(self.local_player.Name)
-    self.land = self.plots:FindFirstChild("Land")
+    self.local_island = self.workspace:WaitForChild("Plots"):WaitForChild(self.local_player.Name)
+    self.land_cube = self.local_island:FindFirstChild("Land")
     self.communication = self.replicated_storage:WaitForChild("Communication")
     
     return self
@@ -40,12 +38,12 @@ function RobloxAPI:GetLocalPlayer()
     return self.local_player
 end
 
-function RobloxAPI:GetPlots()
-    return self.plots
+function RobloxAPI:GetIsland()
+    return self.local_island
 end
 
-function RobloxAPI:GetLand()
-    return self.land
+function RobloxAPI:GetPlatform()
+    return self.land_cube
 end
 
 function RobloxAPI:GetCommunication()
--- modules/UIManager.lua
+++ modules/UIManager.lua
@@ -15,7 +15,7 @@ end
 function UIManager:setup(speed_manager, farm_manager, esp_manager) 
     local window = self.fluent_menu:CreateWindow({
         Title = "ketaminex | ",
-        SubTitle = "dev build: 1.0.74", 
+        SubTitle = "dev build: 1.0.75", 
         TabWidth = 120,
         Size = UDim2.fromOffset(580, 750),
         Theme = "Dark",
