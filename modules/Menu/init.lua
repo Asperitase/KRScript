1827a3d87ac595f311cf088512e8cda7d647b1fb
Asperatise <116976373+Asperitase@users.noreply.github.com>
2025-07-01 20:08:06 +0200

    123

--- modules/Menu/init.lua
+++ modules/Menu/init.lua
@@ -3,6 +3,11 @@
 
 local Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/Asperitase/KRScript/main/modules/Menu/Window.lua?t=" .. tick()))()
 
+local Tab = Window:CreateTab{
+    Title = "Farm",
+    Icon = "leaf"
+}
+
 -- Подгружаем только таб Farm
 local FarmTab = loadstring(game:HttpGet("https://raw.githubusercontent.com/Asperitase/KRScript/main/modules/Farm/init.lua?t=" .. tick()))()
 if FarmTab and FarmTab.Register then FarmTab:Register(Window) end
