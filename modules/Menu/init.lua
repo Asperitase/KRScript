c5caea0797a2826bf87636125813cf65476a8014
Asperatise <116976373+Asperitase@users.noreply.github.com>
2025-07-02 15:31:05 +0200

    1

--- modules/Menu/init.lua
+++ modules/Menu/init.lua
@@ -2,17 +2,16 @@
 -- Точка входа для меню
 
 local FluentMenu = loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/download/v1.0.8/Fluent.luau?t=" .. tick()))()
-local Window = FluentMenu:CreateWindow{
+local Window = FluentMenu:CreateWindow{ 
     Title = "KRScript",
-    SubTitle = "by idredakx",
+    SubTitle = "by idredakx | v1.0.1",
     TabWidth = 160,
     Size = UDim2.fromOffset(830, 525),
     Resize = true,
     MinSize = Vector2.new(470, 380),
     Acrylic = false,
     Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.Q 
+    MinimizeKey = Enum.KeyCode.Q
 }
-
 local FarmTab = loadstring(game:HttpGet("https://raw.githubusercontent.com/Asperitase/KRScript/main/modules/Farm/init.lua?t=" .. tick()))()
 if FarmTab then FarmTab(Window) end 
